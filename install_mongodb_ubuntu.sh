746a1e13a4e2d798e02d1669f4ac81fa63a1ac88
alirezabrtn <alireza.baratian@gmail.com>
2025-01-16 15:22:56 +0330

    Update install_mongodb_ubuntu.sh
    bumped ubuntu to 24.04

--- install_mongodb_ubuntu.sh
+++ install_mongodb_ubuntu.sh
@@ -2,10 +2,10 @@
 
 main() {
 sudo apt-get install gnupg curl
-curl -fsSL https://www.mongodb.org/static/pgp/server-7.0.asc | \
-   sudo gpg -o /usr/share/keyrings/mongodb-server-7.0.gpg \
+curl -fsSL https://www.mongodb.org/static/pgp/server-8.0.asc | \
+   sudo gpg -o /usr/share/keyrings/mongodb-server-8.0.gpg \
    --dearmor
-echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-7.0.gpg ] https://repo.mongodb.org/apt/ubuntu jammy/mongodb-org/7.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-7.0.list
+echo "deb [ arch=amd64,arm64 signed-by=/usr/share/keyrings/mongodb-server-8.0.gpg ] https://repo.mongodb.org/apt/ubuntu noble/mongodb-org/8.0 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-8.0.list
 sudo apt-get update
 sudo apt-get install -y mongodb-org
 echo "mongodb-org hold" | sudo dpkg --set-selections
